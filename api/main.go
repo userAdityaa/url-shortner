0bb510490501def0c8a2d1ee99f2fa6714a31573
Aditya <aditya.chaudhary1558@gmail.com>
2024-09-20 13:55:44 +0530

    feat: Added the docker compose file as required

--- api/main.go
+++ api/main.go
@@ -8,6 +8,7 @@ import (
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/logger"
 	"github.com/joho/godotenv"
+	"github.com/userAdityaa/url-shortner/routes"
 )
 
 func SetUpRoutes(app *fiber.App) {
